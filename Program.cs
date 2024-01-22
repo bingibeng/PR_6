d79f25d9cffdd45f31b47d207f738bb9886fc873
1213-2 <1213-2@1213-2-PC>
2024-01-22 09:07:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПР_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.Title = "Практическая работа № 6"; // заголовок консоли
+
+                int a, b, c; // объявление переменных
+
+                Console.ForegroundColor = ConsoleColor.Yellow; // цвет шрифта
+                Console.BackgroundColor = ConsoleColor.Magenta; // цвет фона шрифта
+                Console.WriteLine("Здравствуйте!");
+
+                Console.Write("Введите a = ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите b = ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите с = ");
+                c = Convert.ToInt32(Console.ReadLine());
+
+                if (a == b || b == c || c == a)
+                    Console.Write("Числа не должны быть равны");
+
+                switch (a > b && a > c && b > c)
+                {
+                    case true:
+                        Console.WriteLine($"Результат: {c}, {b}, {a}"); break;
+                }
+                switch (a > b && a > c && c > b)
+                {
+                    case true:
+                        Console.WriteLine($"Результат: {b}, {c}, {a}"); break;
+                }
+                switch (a > b && a < c && c > b)
+                {
+                    case true:
+                        Console.WriteLine($"Результат: {b}, {a}, {c}"); break;
+                }
+                switch (a < b && b > c && a > c)
+                {
+                    case true:
+                        Console.WriteLine($"Результат: {c}, {a}, {b}"); break;
+                }
+                switch (a < b && b > c && a < c)
+                {
+                    case true:
+                        Console.WriteLine($"Результат: {a}, {c}, {b}"); break;
+                }
+                switch (a < b && b < c && c > a)
+                {
+                    case true:
+                        Console.WriteLine($"Результат: {a}, {b}, {c}"); break;
+                }
+            }
+
+            catch (FormatException) // обработка исключений
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка: введите число, а не символ");
+            }
+            catch (Exception е) // обработка исключений
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка:"+е.Message);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ПР 6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ПР 6")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0b6c4c1d-130a-41d7-8d7d-75c5d5385caa")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
